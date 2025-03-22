2fd94e20ff0d7b7407eb1785f628f2c896efe167
Dithintitus <dithintitus2005@gmail.com>
2025-03-22 06:57:22 +0000

    SJF completed

--- sjf.c
+++ sjf.c
@@ -1,63 +1,125 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 #include<stdlib.h>
 
-struct process{
-    int at,wt,bt,tt,ct,status;
-    char name[5];
-}p[40];
+struct process
+{
+    char name[10];
+    int at,bt,wt,tt,ct,status;
+}p[20];
 
-struct Gantt{
-    char name[5];
+struct done
+{
+    char name[10];
     int st,ct;
-}g[40];
+}d[20];
 
 void main()
 {
-    int i,j,num;
-    float avgwt=0.0, avgtt=0.0;
-
-    printf("Enter the number of processes: ");
-    scanf("%d",&num);
-
-    for(i=0;i<num;i++)
+    int n,min,f,num=0,idle,ls;
+    float totalwt=0,totaltt=0;
+    printf("Enter the limit of process");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
     {
-        printf("\nEnter the name of the process:");
-        scanf("%s",p[i].name);
-        printf("Enter Arrival time:");
-        scanf("%d",&p[i].at);
-        printf("Enter Burst time:");
-        scanf("%d",&p[i].bt);
+        printf("Enter the name of process");
+        scanf("%s", p[i].name);
+        printf("Enter the arrival time and burst time of process");
+        scanf("%d %d",&p[i].at,&p[i].bt);
         p[i].status=0;
     }
-    int min=0,idle=0,fp=0; //fp->found process
-
-    for(i=0;i<num;i++)
+    for(int i=0,ls=0,idle=0;ls<n;)
     {
-        for(j=0;j<num;j++)
+        f=0;
+        for(int j=0;j<n;j++)
         {
-            if(p[j].at<=i&&p[j].status==0)
+            if(p[j].at<=i && p[j].status==0)
             {
-                if(!fp||p[j].bt<p[min].bt)
+                if(!f || p[j].bt<p[min].bt)
                 {
                     min=j;
-                    fp=1;
+                    f=1;
                 }
                 else if(p[j].bt==p[min].bt)
                 {
                     if(p[j].at<p[min].at)
                     {
                         min=j;
-                        fp=1;
+                        f=1;
                     }
                 }
             }
-        }   
+        }
+        if(f) 
+        {
+            if(idle) 
+            { 
+                d[num].ct=i;
+                idle=0;
+                num++;
+            }
+            strcpy(d[num].name,p[min].name);
+            p[min].status=1;
+            d[num].st=i;
+            d[num].ct=i+p[min].bt;
+            i=d[num].ct;
+            p[min].ct=d[num].ct;
+            p[min].tt=p[min].ct-p[min].at;
+            p[min].wt=p[min].tt-p[min].bt;
+            totalwt+=p[min].wt;
+            totaltt+=p[min].tt;
+            num++;
+            ls++;
+        }
+        else 
+        {
+            if(!idle) 
+            {
+                strcpy(d[num].name,"Idle");
+                d[num].st = i;
+                idle = 1;
+            }
+            i++; 
+        }
     }
-    
-    
-    if(fp)
+    int a;
+    printf("\nGantt chart\n");
+    for (int i = 0; i < num; i++) 
     {
-
+    	a=d[i].ct-d[i].st;
+        printf("----");
+        for(int z=1;z<=a;z++) 
+            	printf("-");
     }
+    printf("\n");
+	for (int i = 0; i < num; i++) 
+	{
+	    a=d[i].ct-d[i].st;
+        	printf("|%s", d[i].name);
+        	for (int z = 1; z <=a; z++) 
+            		printf(" ");
+            
+    	}
+    	printf("|\n");
+	for (int i = 0; i < num; i++) 
+	{
+	        a=d[i].ct-d[i].st;
+        	printf("----");
+        	for (int z = 1; z <=a; z++) 
+			    printf("-");
+    	}
+    	printf("\n");
+	for (int i = 0; i < num; i++) 
+	{
+	        a=d[i].ct-d[i].st;
+        	printf("%d", d[i].st);
+        	for (int z = 0; z <=a; z++) 
+            	printf(" ");
+            printf(" ");
+            printf(" ");
+    }
+    printf("%d",d[num-1].ct);
+    printf("\n");
+    printf("Average waiting time: %f\n", totalwt / n);
+    printf("Average turnaround time: %f\n", totaltt / n);
 }
